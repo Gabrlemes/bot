e6101d435ae780b4e49b23ae4614e01fe779dd5a
gabriel <gabrielsiber816@gmail.com>
2022-11-19 10:34:49 -0300

    continuação das tentativas falhas do bot

--- cod.js
+++ cod.js
@@ -1,6 +1,17 @@
+import { join, dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+import { Low } from 'lowdb'
+import { JSONFile } from 'lowdb/node'
+
 const Discord = require("discord.js");
 const cliente = new client.Discord();
 const config = require("./config.json");
+
+const low = require('lowdb') //banco de dados
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('banco.json')
+const db = low(adapter)
 
 client.on("ready", () => {
     console.log(`bot foi iniciado, com ${cliente.users.size} usúarios, em ${cliente.channels.size} canais, em ${cliente.guilds.size} servidores.`);
@@ -8,6 +19,7 @@ client.on("ready", () => {
 });
 
 client.on("guildCreate", guild => {
+    db.set(guild.id, []).write()
     console.log(`o bot entrou no servidor: ${guild.name} (id: ${guild.id}). População: ${guild.memberCount} membros!`);
     client.user.setActivity(`Estou em ${client.guilds.size} servidores.`);
 });
@@ -20,6 +32,7 @@ client.on("guildDelete", guild => {
 client.on("message", async message => {
     if(message.author.bot) return;
     if(message.channel.type === "dm") return;
+    if(!message.content.startsWith(config.prefix)) return;
 
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const comando = args.shift().tolowercase();
@@ -28,7 +41,27 @@ client.on("message", async message => {
       const m = await message.channel.send("ping?");
       m.edit(`pong! a latência é ${m.createdTimestamp - message.createdtimestamp}ms. A Latência da API é %{Math.round(client.ping)}ms`);  
     }
+    if(comando === "criar") {
+        db.get(message.guild.id)
+        .push({
+            id: message.author.id,
+            nick: message.author.id, username,
+            avatar: message.author.displayAvatarURL
+        }).write()
+    }
+    if(comando === "editar"){
+        if(!args[0])return message.channel.send('esqueceu de algo')
+        let [novonome] = args
+        db.get(message.guild.id)
+        .find({id: message.author.id}) .assign({nick: novonome}).write()
+        message.channel.send('perfil atualizado')
+        
+    }
+    if(comando === "apagar"){
+        db.get(message.guild.id).remove({id: message.author.id}).write()
 
+    }
+    
 });
 
 client.login(config.token);
